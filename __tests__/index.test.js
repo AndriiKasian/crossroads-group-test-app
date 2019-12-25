fb61723f9bfd8e7f8fa60cc3a6634dabad57d8ce
Andrii Kasian <a.kasian@videal.net>
2019-12-25 11:04:33 +0200

    [commit list] index page tests udated

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,13 @@
 import React from 'react'
-import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 import App from '../pages/index'
 
-describe('With Enzyme', () => {
-  it('App shows "Hello world!"', () => {
-    const app = shallow(<App />)
+describe('App component', () => {
+  it('should render correctly', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
 
-    expect(app.find('p').text()).toEqual('Hello World!')
+    expect(tree).toMatchSnapshot();
   })
-})
-
-describe('With Snapshot Testing', () => {
-  it('App shows "Hello world!"', () => {
-    const component = renderer.create(<App />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-  })
-})
+});
